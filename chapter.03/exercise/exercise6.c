a3fa34a518737c1a562ac7d6fdaf6594848b3e8c
1615405 <125164331+1615405@users.noreply.github.com>
2023-04-26 15:57:48 +0800

    Update exercise6.c

--- chapter.03/exercise/exercise6.c
+++ chapter.03/exercise/exercise6.c
@@ -13,13 +13,13 @@ in quarts, and displays the number of water molecules in that amount.
 
 int main(void)
 {
-	float H20_MASS = 3.0e-23;
-	float GRAMS_H20_PER_QUART = 950.;
-	float quarts;
+    float H20_MASS = 3.0e-23;
+    float GRAMS_H20_PER_QUART = 950.;
+    float quarts;
 
-	printf("Enter an amount of water (in quarts): ");
-	scanf("%f", &quarts);
-	printf("There are %f molecules in %f quarts of water.\n", quarts * GRAMS_H20_PER_QUART / H20_MASS, quarts);
+    printf("Enter an amount of water (in quarts): ");
+    scanf("%f", &quarts);
+    printf("There are %f molecules in %f quarts of water.\n", quarts * GRAMS_H20_PER_QUART / H20_MASS, quarts);
 
-	return 0;
+    return 0;
 }
