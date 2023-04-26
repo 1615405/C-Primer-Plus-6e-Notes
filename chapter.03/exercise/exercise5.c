2c45c5ffb9647df864e45afe6ac846c4b10757a4
1615405 <125164331+1615405@users.noreply.github.com>
2023-04-26 15:57:15 +0800

    Update exercise5.c

--- chapter.03/exercise/exercise5.c
+++ chapter.03/exercise/exercise5.c
@@ -12,12 +12,12 @@ your age in years and then displays the equivalent number of seconds.
 
 int main(void)
 {
-	unsigned int SECONDS_PER_YEAR = 31560000;
-	unsigned int age;
+    unsigned int SECONDS_PER_YEAR = 31560000;
+    unsigned int age;
 
-	printf("What is your age (in years)?: ");
-	scanf("%u", &age);
-	printf("You are %u seconds old!\n", SECONDS_PER_YEAR * age);
+    printf("What is your age (in years)?: ");
+    scanf("%u", &age);
+    printf("You are %u seconds old!\n", SECONDS_PER_YEAR * age);
 
-	return 0;
+    return 0;
 }
