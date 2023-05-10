5684dcf0a0dcad766dcdcd5adf836f4d0f0152e2
1615405 <125164331+1615405@users.noreply.github.com>
2023-05-10 19:58:43 +0800

    Update exercise2.c

--- chapter.04/exercise/exercise2.c
+++ chapter.04/exercise/exercise2.c
@@ -14,16 +14,16 @@ d. Prints it in a field three characters wider than the name */
 
 int main(void)
 {
-	char name[20];
-	int name_length;
+    char name[20];
+    int name_length;
 
-	printf("Enter your first name: ");
-	scanf("%s", name);
-	name_length = strlen(name);
-	printf("\"%s\"\n", name);                    // a. enclosed in double quotes
-	printf("\"%20s\"\n", name);                  // b. double quotes, 20 char wide, right-justified
-	printf("\"%-20s\"\n", name);                 // c. double quotes, 20 char wide, left-justified
-	printf("\"%*s\"\n", name_length + 3, name);  // d. double quotes, 3 char wider than name
+    printf("Enter your first name: ");
+    scanf("%s", name);
+    name_length = strlen(name);
+    printf("\"%s\"\n", name);                    // a. enclosed in double quotes
+    printf("\"%20s\"\n", name);                  // b. double quotes, 20 char wide, right-justified
+    printf("\"%-20s\"\n", name);                 // c. double quotes, 20 char wide, left-justified
+    printf("\"%*s\"\n", name_length + 3, name);  // d. double quotes, 3 char wider than name
 
-	return 0;
+    return 0;
 }
