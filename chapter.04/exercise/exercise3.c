e6fb343119406f263c0e9254eeefcb926e19c1c1
1615405 <125164331+1615405@users.noreply.github.com>
2023-05-10 19:59:17 +0800

    Update exercise3.c

--- chapter.04/exercise/exercise3.c
+++ chapter.04/exercise/exercise3.c
@@ -14,12 +14,12 @@ b. The input is +21.290 or 2.129E+001. */
 
 int main(void)
 {
-	float num;
+    float num;
 
-	printf("Enter a number: ");
-	scanf("%f", &num);
-	printf("The input is %.1f or %.1e\n", num, num);
-	printf("The input is %+.3f or %.3E\n", num, num);
+    printf("Enter a number: ");
+    scanf("%f", &num);
+    printf("The input is %.1f or %.1e\n", num, num);
+    printf("The input is %+.3f or %.3E\n", num, num);
 
-	return 0;
+    return 0;
 }
