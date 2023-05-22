866291f94ea4ff627927d2b80b9e1c140044767b
1615405 <1615405@qq.com>
2023-05-23 07:55:31 +0800

    add files

---
+++ chapter.07/exercise/exercise01.c
@@ -0,0 +1,34 @@
+/*
+ * C Primer Plus Sixth Edition
+ *
+ * Chapter 7 Exercise 1:
+ * 
+ * Write a program that reads input until encountering the # character and then
+ * reports the number of spaces read, the number of newline characters read,
+ * and the number of all other characters read.
+*/
+
+
+#include <stdio.h>
+
+#define STOP '#'
+
+int main(void)
+{
+    char ch;
+    unsigned int spaces = 0, newlines = 0, other = 0;
+    printf("Enter input (%c to stop):\n", STOP);
+    while ((ch = getchar()) != STOP)
+    {
+        if (ch == ' ')
+            spaces++;
+        else if (ch == '\n')
+            newlines++;
+        else
+            other++;
+    }
+    printf("Character Count:\n");
+    printf("----------------\n");
+    printf("Spaces: %u, Newlines: %u, Other: %u\n", spaces, newlines, other);
+    return 0;
+}
