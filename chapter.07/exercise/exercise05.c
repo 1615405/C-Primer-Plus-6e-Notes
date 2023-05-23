c136ab05c5293b76a1490d0682bd044d7d7f4ae9
1615405 <1615405@qq.com>
2023-05-23 16:05:46 +0800

    add files

---
+++ chapter.07/exercise/exercise05.c
@@ -0,0 +1,36 @@
+/* 
+ * C Primer Plus Sixth Edition
+ *
+ * Chapter 7 Exercise 5:
+ * 
+ * Redo exercise 4 using a switch.
+*/
+
+
+#include <stdio.h>
+
+#define STOP '#'
+
+int main(void)
+{
+    char ch;
+
+    printf("Enter input (%c to exit):\n", STOP);
+    while ((ch = getchar()) != STOP)
+    {
+        switch (ch)
+        {
+            case '.':
+                printf("!");
+                break;
+            case '!':
+                printf("!!");
+                break;
+            default:
+                printf("%c", ch);
+                break;
+        }
+    }
+
+    return 0;
+}
