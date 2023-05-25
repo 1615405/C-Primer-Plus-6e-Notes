9df0c8313ef27b6836798c71a1911f7b63083a27
1615405 <1615405@qq.com>
2023-05-25 21:25:34 +0800

    update

--- chapter.05/listing/pound.c
+++ chapter.05/listing/pound.c
@@ -23,14 +23,3 @@ void pound(int n)         // ANSI-style function header
         printf("#");
     printf("\n");
 }
-
-
-/**
- * Variable names are private to the function. This means that a name defined in one function doesn't conflict with the
- * same name defined elsewhere.
- * 
- * A prototype is a function declaration that describes a function's return value and its arguments. This prototype says
- * two things about the pound() function.
- *     The function has no return value
- *     The function takes one argument, which is a type int value.
-*/
