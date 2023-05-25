bd743af266c9f8b96f1ea1f21922f193201476be
1615405 <1615405@qq.com>
2023-05-25 10:38:53 +0800

    update

--- chapter.03/exercise/exercise3.c
+++ chapter.03/exercise/exercise3.c
@@ -12,7 +12,7 @@
 
 int main(void)
 {
-    printf("\a"); // sound alert
+    printf("\a");
     printf("Startled by the sudden sound, Sally shouted, \"By the Great Pumpkin, what was that!\"\n");
 
     return 0;
