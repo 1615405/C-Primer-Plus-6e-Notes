aef003a5cd2a9c5cd0aa07f77f5767d830779021
1615405 <1615405@qq.com>
2023-05-27 09:58:34 +0800

    add files

---
+++ chapter.08/listing/file_eof.c
@@ -0,0 +1,25 @@
+/* file_eof.c -- open a file and display it */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int ch;
+    FILE* fp;
+    char fname[30];
+
+    printf("Enter the name of the file: ");
+    scanf("%s", fname);
+    fp = fopen(fname, "r");
+    if (fp == NULL) {
+        printf("Failed to open file. Bye\n");
+        exit(1);
+    }
+    while ((ch = getc(fp)) != EOF) {
+        putchar(ch);
+    }
+    fclose(fp);
+
+    return 0;
+}
