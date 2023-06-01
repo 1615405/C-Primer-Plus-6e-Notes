d1fa0be7deade2458af8b66b99d7028b313ceb48
1615405 <1615405@qq.com>
2023-06-01 22:57:29 +0800

    Update toobig.c

--- chapter.03/listing/toobig.c
+++ chapter.03/listing/toobig.c
@@ -1,12 +1,5 @@
 /* toobig.c -- exceeds maximum int size on your system */
 
-/**
- * The unsigned integer j is acting like a car's odometer. when it reaches its maximum value, it starts over at the
- * beginning. The integer i acts similarly. The main difference is that the unsigned int variable j, like an odometer,
- * begins at 0, but the int variable i begins at -2147483648.
-*/
-
-
 #include <stdio.h>
 
 int main(void)
