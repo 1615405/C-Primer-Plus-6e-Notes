57942afb06242da352b7eabb4c0e32301a8ae8d0
1615405 <1615405@qq.com>
2023-06-05 23:40:03 +0800

    Update exercise3.c

--- chapter.02/exercise/exercise3.c
+++ chapter.02/exercise/exercise3.c
@@ -5,7 +5,7 @@ int main(void)
     int age, days;
     const int ONE_YEAR = 365;
 
-    printf("please input your age : \n");
+    printf("please input your age: \n");
     scanf("%d", &age);
     days = age * ONE_YEAR;
     printf("you live %d days\n", days);
